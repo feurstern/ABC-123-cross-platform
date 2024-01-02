8124b1f1ec77ced39a8f7a2d2ac21be721660a41
feurstern <naziincolour123@gmail.com>
2024-01-02 11:08:19 +0700

    commit after the fucking error

--- main-v1.js
+++ main-v1.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, screen, globalShortcut, Menu, shell } = require('ele
 const os = require('os');
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const baseUrl = 'https://cbt.ourtoga.com/login';
+const isMac = process.platform === 'darwin';
 
 let mainWindow;
 
@@ -13,9 +14,13 @@ let disableKeysList = ['Alt+Tab', 'F11']
 
 
 const createWindow = () => {
+
+    const display = screen.getPrimaryDisplay();
+    const { height, width } = display.workAreaSize;
+
     mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800,
+        height: height,
+        width: width,
         fullscreen: true,
         resizable: false,
         webPreferences: {
@@ -23,27 +28,150 @@ const createWindow = () => {
         }
     });
 
-    mainWindow.loadURL(baseUrl); 
+    mainWindow.loadURL(baseUrl);
 
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
 
+    // currentDisplay();
+    // this.externalDisplay();
+
 }
 
 app.on('ready', () => {
-    createWindow();
 
+    createWindow();
+    createMenu();
     // disable the key when the window has been succesfully loaded hehehe
     disableKeysList.forEach(key => {
         globalShortcut.register(key, () => {
             console.log(`${key} is disabled!`);
-
         })
     });
-    
-
-
 })
 
+
+// to detect if the user ha s more than one displays
+function externalDisplay() {
+    const display = screen.getAllDisplays();
+    const external = display.find(display => {
+        if (external != null) {
+            return console.log('External display:', externalDisplay);
+        }
+        else {
+            return console.log('No secondary display is detected!')
+        }
+    })
+}
+
+const createMenu = () => {
+    const template = [
+        // { role: 'appMenu' }
+        ...(isMac
+            ? [{
+                label: app.name,
+                submenu: [
+                    { role: 'about' },
+                    { type: 'separator' },
+                    { role: 'services' },
+                    { type: 'separator' },
+                    { role: 'hide' },
+                    { role: 'hideOthers' },
+                    { role: 'unhide' },
+                    { type: 'separator' },
+                    { role: 'quit' }
+                ]
+            }]
+            : []),
+        // { role: 'fileMenu' }
+        {
+            label: 'File',
+            submenu: [
+                isMac ? { role: 'close' } : { role: 'quit' }
+            ]
+        },
+        // { role: 'editMenu' }
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                ...(isMac
+                    ? [
+                        { role: 'pasteAndMatchStyle' },
+                        { role: 'delete' },
+                        { role: 'selectAll' },
+                        { type: 'separator' },
+                    ]
+                    : [
+                        { role: 'delete' },
+                        { type: 'separator' },
+                        { role: 'selectAll' }
+                    ])
+            ]
+        },
+        // { role: 'viewMenu' }
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forceReload' },
+                { role: 'toggleDevTools' },
+                { type: 'separator' },
+                { role: 'resetZoom' },
+                { role: 'zoomIn' },
+                { role: 'zoomOut' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        // { role: 'windowMenu' }
+        {
+            label: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'zoom' },
+                ...(isMac
+                    ? [
+                        { type: 'separator' },
+                        { role: 'front' },
+                        { type: 'separator' },
+                        { role: 'window' }
+                    ]
+                    : [
+                        { role: 'close' }
+                    ])
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'About US',
+                    click: async () => {
+                        await shell.openExternal('https://ourtoga.com/')
+                    }
+                }
+            ]
+        }
+    ]
+
+    const menu = Menu.buildFromTemplate(template);
+    console.log('hehehe');
+    return Menu.setApplicationMenu(menu);
+}
+
+
+app.on('window-all-closed', () => {
+    if (!isMac) app.quit;
+});
+
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
+})
 
