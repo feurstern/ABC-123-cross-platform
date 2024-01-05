019808014d9335b560563f02e32579f5cc47ca10
Rio-feuerstern <naziincolour123@gmail.com>
2024-01-05 15:08:29 +0700

    disable any multiple screen during opening cbt

--- main-v1.js
+++ main-v1.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow, screen, globalShortcut, Menu, shell } = require('electron')
+const { app, BrowserWindow, screen, globalShortcut, Menu, shell } = require('electron');
+const EventEmitter = require('node:events');
+// const { platform } = require('node:os');
+const { eventNames } = require('node:process');
 const os = require('os');
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const baseUrl = 'https://cbt.ourtoga.com/login';
@@ -9,8 +12,9 @@ let mainWindow;
 // checking whetheer is still on dev or live 
 console.log('isDev', isDevelopment);
 // the list of keys that wiil be disabled during the process
-let disableKeysList = ['Alt+Tab', 'F11']
-// console.log('current OS :', os);
+let disableKeysList = ['Alt+Tab', 'F11', 'Cmd+Tab'];
+process.platform ='linux'
+console.log('current os:', process.platform);
 
 
 const createWindow = () => {
@@ -23,26 +27,32 @@ const createWindow = () => {
         width: width,
         fullscreen: true,
         resizable: false,
+        frame:false,
         webPreferences: {
             nodeIntegration: true
         }
     });
 
     mainWindow.loadURL(baseUrl);
+    // mainWindow.setEnabled(false);
 
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
 
+    mainWindow.setEnabled(false);
+
     // currentDisplay();
     // this.externalDisplay();
 
+    // console.log('event emitter', EventEmitter);
 }
 
 app.on('ready', () => {
 
     createWindow();
     createMenu();
+    // externalDisplay();
     // disable the key when the window has been succesfully loaded hehehe
     disableKeysList.forEach(key => {
         globalShortcut.register(key, () => {
@@ -52,23 +62,20 @@ app.on('ready', () => {
 })
 
 // to detect if the user ha s more than one displays
-function externalDisplay() {
-    const display = screen.getAllDisplays();
-    const external = display.find(display => {
-        if (external != null) {
-            return console.log('External display:', externalDisplay);
-        }
-        else {
-            return console.log('No secondary display is detected!')
-        }
-    })
-}
+// function externalDisplay() {
+//     const display = screen.getAllDisplays();
+//     const external = display.find(display => {
+//         if (external != null) {
+//             return console.log('External display:', externalDisplay);
+//         }
+//         else {
+//             return console.log('No secondary display is detected!')
+//         }
+//     })
+// }
 
 const createMenu = () => {
     
-}
-
-
 const template = [
     // { role: 'appMenu' }
     ...(isMac
@@ -157,6 +164,7 @@ const template = [
             {
                 label: 'About US',
                 click: async () => {
+                    // alert('You have been clicked the button!');
                     await shell.openExternal('https://ourtoga.com/')
                 }
             }
@@ -165,7 +173,13 @@ const template = [
 ]
 
 const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
+ return Menu.setApplicationMenu(menu)
+
+}
+
+app.on('activate', ()=>{
+    
+})
 
 
 app.on('window-all-closed', () => {
@@ -176,3 +190,6 @@ app.on('will-quit', () => {
     globalShortcut.unregisterAll();
 })
 
+
+
+// console.log()
